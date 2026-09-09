f78ea58d4b62e589079c1a1cd3aab301eebfd9bf
Banty Kumar <158995119+Banty825413@users.noreply.github.com>
2026-09-09 12:01:38 +0530

    Time: 328 ms (30.97%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,11 @@
-SELECT
-    (SELECT DISTINCT salary
-     FROM Employee
-     ORDER BY salary DESC
-     LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+SELECT 
+
+(
+    SELECT salary  
+        FROM( 
+            SELECT salary , 
+                            DENSE_RANK()  OVER (ORDER BY salary DESC) rnk
+            FROM Employee
+            ) AS ranked
+        WHERE rnk = 2
+LIMIT  1) AS  SecondHighestSalary
