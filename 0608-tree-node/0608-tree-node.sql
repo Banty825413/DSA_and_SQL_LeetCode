4729dec9555cbd151b5bf2957011c21a867fba9f
Banty Kumar <158995119+Banty825413@users.noreply.github.com>
2026-09-11 15:37:23 +0530

    Time: 472 ms (86.67%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,7 @@
+select id , 
+        case 
+        when p_id is null then "Root"
+        when id in (select distinct p_id from Tree where p_id is not null) then "Inner"
+        else "Leaf"
+        end As Type
+        from Tree
