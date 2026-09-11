1eabc6a64ecc0e73fd8795dbeceb7c432d6c4cb2
Banty Kumar <158995119+Banty825413@users.noreply.github.com>
2026-09-11 09:06:16 +0530

    Time: 606 ms (69.28%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,12 @@
+SELECT  ROUND(SUM(tiv_2016),2) as tiv_2016 
+FROM Insurance
+WHERE tiv_2015 in (
+    SELECT tiv_2015 from Insurance 
+    group by tiv_2015
+    having count(*) >1
+)
+AND (lat ,lon )IN (
+    SELECT lat, lon FROM Insurance 
+    GROUP BY lat,lon 
+    HAVING COUNT(*) = 1 
+)
