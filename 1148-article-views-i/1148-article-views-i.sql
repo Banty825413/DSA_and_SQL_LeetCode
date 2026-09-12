3ceb21e9c435e58e8c8a5f69540f37f848d0c687
Banty Kumar <158995119+Banty825413@users.noreply.github.com>
2026-09-12 12:28:42 +0530

    Time: 459 ms (53.7%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,3 +1,3 @@
-select distinct v1.author_id  as id from Views v1 join Views v2 on v1.author_id = v2.viewer_id 
-where v1.author_id = v2.viewer_id
-order by v1.author_id asc 
+select distinct author_id as id from Views 
+where author_id = viewer_id 
+order by id 
